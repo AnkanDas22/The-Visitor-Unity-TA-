8e6c8454e80f97fd7d98b8dd62ea67d8f4323f52
AnkanDas <ankandas2222@gmail.com>
2019-07-13 04:22:33 +0530

    A test, I guess.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] State initialState;
+    [SerializeField] TextMeshProUGUI storytext;
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = initialState;
+        storytext.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageStates();
+    }
+
+    void ManageStates()
+    {
+        State[] nextstates = state.GetNextStates();
+        for (int i = 0; i < nextstates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextstates[i];
+            }
+        }
+        storytext.text = state.GetStateStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14,10)][SerializeField] string text;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return text;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
